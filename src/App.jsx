47c7f43453cf6e9d0d2c517535a7f9fd91bb5128
shrey94 <shrey.94@hotmail.com>
2025-03-29 14:02:25 -0400

    added fakeauthentication to understand how it will work in the real world added a protected route to not allow access to main app without logging in first

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,43 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   //giving access to cities at the highest level so that we can pass the data along to the pages
 
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />}></Route>
-          <Route path="product" element={<Product />}></Route>
-          <Route path="pricing" element={<Pricing />}></Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<CityList />}></Route>
-            <Route path="cities" element={<CityList />}></Route>
-            <Route path="cities/:id" element={<City />}></Route>
-            <Route path="countries" element={<CountryList />}></Route>
-            <Route path="form" element={<Form />}></Route>
-          </Route>
-          <Route path="*" element={<PageNotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />}></Route>
+            <Route path="product" element={<Product />}></Route>
+            <Route path="pricing" element={<Pricing />}></Route>
+
+            <Route path="login" element={<Login />}></Route>
+
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<CityList />}></Route>
+              <Route path="cities" element={<CityList />}></Route>
+              <Route path="cities/:id" element={<City />}></Route>
+              <Route path="countries" element={<CountryList />}></Route>
+              <Route path="form" element={<Form />}></Route>
+            </Route>
+
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./User.module.css";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
+// const FAKE_USER = {
+//   name: "Jack",
+//   email: "jack@example.com",
+//   password: "qwerty",
+//   avatar: "https://i.pravatar.cc/100?u=zz",
+// };
 
 function User() {
-  const user = FAKE_USER;
+  const { user, logout } = useAuth();
+  console.log(user);
+  const navigate = useNavigate();
 
-  function handleClick() {}
+  // const user = FAKE_USER;
+
+  function handleClick() {
+    // e.preventDefault();
+
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
